03548cba12163ceb32917bd17b01e891b5e10999
KMulloth <k.mulloth@gmail.com>
2023-08-15 11:27:22 -0500

    email fix

--- email.php
+++ email.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
 
     // Send an email to the site owner
-    $to = "your@email.com"; // Replace with your email address
+    $to = "kmulloth@gmail.com"; // Replace with your email address
     $subject = "New Contact Form Submission";
     $headers = "From: $email";
 
